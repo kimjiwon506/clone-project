f28de51824267d2bdb0689809f392d8ee2652302
kimjiwon506 <kimjiwon506@gmail.com>
2023-12-05 15:27:40 +0900

    test

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -9,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
 export const appRouter = createTRPCRouter({
   post: postRouter,
 });
+// test
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
